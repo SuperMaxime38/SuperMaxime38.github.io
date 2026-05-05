08898d626a3373d33a80ad6735bb428b395e5e83
SuperMaxime38 <max.cardot.clement@icloud.com>
2026-05-05 21:45:29 +0200

    some edits :)

--- helper.js
+++ helper.js
@@ -1,7 +1,26 @@
-function display(toDisplay) {
-    let selection = document.getElementById(toDisplay);
-    selection.style.display = "block";
-}
+// function display(toDisplay) {
+//     let selection = document.getElementById(toDisplay);
+//     selection.style.display = "block";
+// }
+
+// var desc_child_clicked = false; // declare the variable that tracks the state
+// function desc_child_handler(){ // declare a function that updates the state
+//   desc_child_clicked = true;
+// }
+
+// var desc_child = document.getElementById('desc_child'); // grab a reference to your element
+// desc_child.addEventListener('click', desc_child_handler);
+
+// function hide(toHide) {
+//     let selection = document.getElementById(toHide);
+
+//     if(!desc_child_clicked){
+//         selection.style.display = "none";
+//     }
+    
+//     desc_child_clicked = false;
+// }
+
 
 var desc_child_clicked = false; // declare the variable that tracks the state
 function desc_child_handler(){ // declare a function that updates the state
@@ -11,12 +30,34 @@ function desc_child_handler(){ // declare a function that updates the state
 var desc_child = document.getElementById('desc_child'); // grab a reference to your element
 desc_child.addEventListener('click', desc_child_handler);
 
-function hide(toHide) {
-    let selection = document.getElementById(toHide);
+// LOGIN POPUP
+let isDescOpen = false;
+opened = "";
 
-    if(!desc_child_clicked){
-        selection.style.display = "none";
+function display(toDisplay) {
+    if(opened != "") {
+        hide();
     }
-    
-    desc_child_clicked = false;
+    // Set the height of the popup to 800px
+    document.getElementById(toDisplay).style.height = "100%";
+    // Display the navigation bar
+    document.getElementById(toDisplay).style.display = "block";
+    isDescOpen = true;
+    opened = toDisplay;
+
+}
+
+
+function hide() {
+    if(!desc_child_clicked){
+        // Set the height of the popup to 0
+        document.getElementById(opened).style.height = "0";
+        // Hide the navigation bar
+        document.getElementById(opened).style.display = "none";
+        isDescOpen = false;
+        opened = "";
+    } else {
+        desc_child_clicked = false;
+    }
+  
 }
