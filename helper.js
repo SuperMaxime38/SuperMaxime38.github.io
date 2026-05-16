3e8e290d86df8725d47d6bd409efc9b7ffe89e87
SuperMaxime38 <max.cardot.clement@icloud.com>
2026-05-16 17:28:34 +0200

    CSS update + round PP
    title
    + fixed js bug
    = transformed name in navbar to a top link

--- helper.js
+++ helper.js
@@ -27,17 +27,31 @@ function desc_child_handler(){ // declare a function that updates the state
   desc_child_clicked = true;
 }
 
-var desc_child = document.getElementById('desc_child'); // grab a reference to your element
-desc_child.addEventListener('click', desc_child_handler);
+var chatbtp_desc_child = document.getElementById('chatbtp_desc_child');
+chatbtp_desc_child.addEventListener('click', desc_child_handler);
+
+var s1_04_desc_child = document.getElementById('s1_04_desc_child');
+s1_04_desc_child.addEventListener('click', desc_child_handler);
+
+var youtoube_desc_child = document.getElementById('youtoube_desc_child');
+youtoube_desc_child.addEventListener('click', desc_child_handler);
+
+var a_mazing_game_desc_child = document.getElementById('a_mazing_game_desc_child');
+a_mazing_game_desc_child.addEventListener('click', desc_child_handler);
+
+var video_to_ascii_desc_child = document.getElementById('video_to_ascii_desc_child');
+video_to_ascii_desc_child.addEventListener('click', desc_child_handler);
+
+var mctdl_desc_child = document.getElementById('mctdl_desc_child');
+mctdl_desc_child.addEventListener('click', desc_child_handler);
+
+
 
 // LOGIN POPUP
 let isDescOpen = false;
 opened = "";
 
 function display(toDisplay) {
-    if(opened != "") {
-        hide();
-    }
     // Set the height of the popup to 800px
     document.getElementById(toDisplay).style.height = "100%";
     document.getElementById(toDisplay).style.display = "flex";
